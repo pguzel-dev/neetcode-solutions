802dd6973fdf105c349551b17a2fb7a07121374b
Poyraz Guzel <poyrazzguzel@gmail.com>
2026-04-02 23:26:12 -0400

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        solutions = [1] * len(nums)
+
+        prefix = 1
+        for i in range(len(nums)):
+            solutions[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            solutions[i] *= postfix
+            postfix *= nums[i]
+
+        return solutions
