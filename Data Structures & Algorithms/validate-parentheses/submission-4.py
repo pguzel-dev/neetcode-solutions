391fd53d32f873dbd294033fcf999c146eec00c7
Poyraz Guzel <poyrazzguzel@gmail.com>
2026-04-04 21:52:27 -0400

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,27 @@
+# Solution with dicts and shit
+class Solution:
+    def isValid(self, s: str) -> bool:
+        bracket_map = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+        stack = []
+
+        for char in s:
+            # check for '(' '[' '{'
+            if char in bracket_map.values():
+                stack.append(char)
+            # check for ')' ']' '}'
+            elif bracket_map:
+                if not stack:
+                    return False
+                popped_char = stack.pop()
+                # popped char must be equal to opening char
+                # left side of the equation is closing and 
+                # right side of the equation is opening
+                if popped_char != bracket_map[char]:
+                    return False
+
+        return len(stack) == 0   # ensure no leftovers
+        
