6e1483bdb9fce9ed84a3984309fc870f4596e52d
Poyraz Guzel <poyrazzguzel@gmail.com>
2026-04-04 21:43:37 -0400

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,21 @@
+# Time: O(n)
+# Space: O(n)
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        for char in s:
+            if char == '(' or char == '{' or char == '[':
+                stack.append(char)
+            elif char == ')' or char == '}' or char == ']':
+                if not stack:   # avoid popping empty stack
+                    return False
+                popped_char = stack.pop()
+                if char == ')' and popped_char != '(':
+                    return False
+                elif char == '}' and popped_char != '{':
+                    return False
+                elif char == ']' and popped_char != '[':
+                    return False
+
+        return len(stack) == 0   # ensure no leftovers
