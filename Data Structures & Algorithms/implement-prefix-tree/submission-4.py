e5a5ee2a756f83047b77f2a597281f55201b7d11
Poyraz Guzel <poyrazzguzel@gmail.com>
2026-05-12 13:24:09 -0400

    Add: implement-prefix-tree - submission-4

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-4.py
@@ -0,0 +1,48 @@
+# insert:
+# Time: O(n)
+# Space: O(n) in worst case for new nodes
+
+# search:
+# Time: O(n)
+# Space: O(1)
+
+# startsWith:
+# Time: O(n)
+# Space: O(1)
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endWord = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        current = self.root
+
+        for char in word:
+            if char not in current.children:
+                current.children[char] = TrieNode()
+            current = current.children[char]
+        
+        current.endWord = True
+
+    def search(self, word: str) -> bool:
+        current = self.root
+        for char in word:
+            if char not in current.children:
+                return False
+            current = current.children[char]
+        return current.endWord
+
+    def startsWith(self, prefix: str) -> bool:
+        current = self.root
+        for char in prefix:
+            if char not in current.children:
+                return False
+            current = current.children[char]
+        return True
+        
