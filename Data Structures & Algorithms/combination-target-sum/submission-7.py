50cd74de0fdc92b97d6980659a215679557b8e22
Poyraz Guzel <poyrazzguzel@gmail.com>
2026-05-11 22:06:21 -0400

    Add: combination-target-sum - submission-7

---
+++ Data Structures & Algorithms/combination-target-sum/submission-7.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        results = []
+        curr = []
+
+        def backtrack(i):
+            # Base cases, to stop paths
+            if sum(curr) == target:
+                results.append(curr.copy())
+                return
+            if sum(curr) > target:
+                return
+            if i == len(nums):
+                return
+            
+            # tracking
+            curr.append(nums[i])    # add curr num
+            backtrack(i)            # reuse in next
+            curr.pop()              # take out curr num
+            backtrack(i + 1)        # skip out curr num
+            i += 1                  # iterate to next num
+
+        backtrack(0)
+        return results
