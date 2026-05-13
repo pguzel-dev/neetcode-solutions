922bb02e8d267481f4d468031ab1b26497151541
Poyraz Guzel <poyrazzguzel@gmail.com>
2026-05-13 12:31:43 -0400

    Add: house-robber-ii - submission-10

---
+++ Data Structures & Algorithms/house-robber-ii/submission-10.py
@@ -0,0 +1,16 @@
+# Time: O(n)
+# Space: O(n)
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        def simple_rob(loot: List[int]) -> int:
+            loot.append(0)
+    
+            for index in range(len(loot) - 3, -1, -1):
+                loot[index] = max(loot[index] + loot[index + 2], loot[index + 1])
+
+            return loot[0]
+        
+        return max(simple_rob(nums[:-1]), simple_rob(nums[1:]))
