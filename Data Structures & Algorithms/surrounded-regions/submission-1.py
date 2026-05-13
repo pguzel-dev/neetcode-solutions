ca4d97310d61fe7243ec0cf917e486d5bffc847f
Poyraz Guzel <poyrazzguzel@gmail.com>
2026-05-13 00:07:59 -0400

    Add: surrounded-regions - submission-1

---
+++ Data Structures & Algorithms/surrounded-regions/submission-1.py
@@ -0,0 +1,39 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ROWS, COLS = len(board), len(board[0])
+        q = deque()
+        reachable = set()
+
+        def update(r: int, c: int):
+            if (
+                r < 0 or c < 0 or
+                r >= ROWS or c >= COLS or
+                board[r][c] != "O" or
+                (r,c) in reachable
+            ):
+                return
+
+            q.append([r,c])
+            reachable.add((r,c))
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (board[r][c] == "O" and
+                    (r == 0 or r == ROWS-1
+                    or c == 0 or c == COLS-1)):
+                    q.append([r,c])
+                    reachable.add((r,c))
+        
+        while q:
+            for i in range(len(q)):
+                r, c = q.popleft()
+
+                update(r + 1, c)
+                update(r - 1, c)
+                update(r, c + 1)
+                update(r, c - 1)
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O" and (r, c) not in reachable:
+                    board[r][c] = "X"
