a4b9d34556a7927e99f44e206e9dcec4685544e0
Poyraz Guzel <poyrazzguzel@gmail.com>
2026-05-13 13:10:42 -0400

    Add: single-number - submission-3

---
+++ Data Structures & Algorithms/single-number/submission-3.py
@@ -0,0 +1,10 @@
+# Time: O(n)
+# Space: O(1)
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0 # n ^ 0 = n
+
+        for num in nums:
+            res = num ^ res 
+        
+        return res
