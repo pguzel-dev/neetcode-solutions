c6d942b10559bcc2b8fbf0fa90c3eff17a3a15e9
Poyraz Guzel <poyrazzguzel@gmail.com>
2026-05-13 12:31:15 -0400

    Add: house-robber-ii - submission-9

---
+++ Data Structures & Algorithms/house-robber-ii/submission-9.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def houseRobber1(loot):
+            prev2 = 0
+            prev1 = 0
+
+            for money in loot:
+                new_best = max(prev1, prev2 + money)
+                prev2 = prev1
+                prev1 = new_best
+
+            # By the time the loop ends, the “current house” is the last house
+            return prev1
+
+        if len(nums) == 1:
+            return nums[0]
+
+        return max(
+            houseRobber1(nums[1:]),
+            houseRobber1(nums[:-1])
+        )
