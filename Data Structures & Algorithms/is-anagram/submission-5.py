d65f1258b6708eaf0a365830737acd32a345cfe8
Poyraz Guzel <poyrazzguzel@gmail.com>
2026-05-19 13:05:30 -0400

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,15 @@
+# Time: O(n + m)
+# Space: O(n + m)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_seen, t_seen= {}, {}
+
+        for val in s:
+            s_seen[val] = s_seen.get(val, 0) + 1
+        for val in t:
+            t_seen[val] = t_seen.get(val, 0) + 1
+        
+        return s_seen == t_seen
